6842993457f3488f0a29133c7030c3d63fab450b
0Nott <jixiao1@s.sfusd.edu>
2024-04-02 22:59:40 -0700

    Finished M5L5

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Area2D
 
 
 var max_speed := 1200.0
@@ -20,4 +20,22 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 
 	if velocity.length() > 0.0:
-		rotation = velocity.angle()
+		#rotation = velocity.angle()
+		get_node("Sprite2D").rotation = velocity.angle()
+		
+var health := 10
+
+func _ready()-> void:
+	set_health(50)
+	area_entered.connect(_on_area_entered)
+	# This call updates the health bar to match the health variable when the
+	# game starts.
+	set_health(health)
+
+func set_health(new_health: int) -> void:
+	health = new_health
+	get_node("UI/HealthBar").value = health
+	
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	set_health(health + 10)
+	
